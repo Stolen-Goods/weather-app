5cdcf4d15fbaa8a51f39dc431705969ab0caef0f
hurley04155@gmail.com <hurley04155@gmail.com>
2025-05-20 16:44:40 -0400

    Removed previous data display

--- script.js
+++ script.js
@@ -12,21 +12,40 @@ async function resolve(location) {
     const { feelslike, conditions, precip, snow } =
       weatherData.currentConditions;
     const { address } = weatherData;
+    let weatherCondition = "";
+
+    if (conditions.toLowerCase().includes("clear")) {
+      weatherCondition =
+        "https://media.tenor.com/XImGzqlnkNIAAAAj/sun-smile.gif";
+    } else if (
+      conditions.toLowerCase().includes("cloudy") ||
+      conditions.toLowerCase().includes("overcast")
+    ) {
+      weatherCondition =
+        "https://media.tenor.com/7ymwjVfhJPwAAAAi/clouds-happy.gif";
+    }
+
     const cityData = document.createElement("div");
     cityData.innerHTML = `
     <h1>${address}</h1>
     <h3>${feelslike}*F</h3>
-    <img src=${
-      conditions === "Clear"
-        ? "https://media.tenor.com/XImGzqlnkNIAAAAj/sun-smile.gif"
-        : "https://media.tenor.com/7ymwjVfhJPwAAAAi/clouds-happy.gif"
-    } width="350px" height="auto">
+    <img src=${weatherCondition} width="350px" height="auto">
     `;
     cityInfoContainer.appendChild(cityData);
-    console.log(weatherData);
+    console.log(conditions);
   } catch {}
 }
 
-searchBtn.addEventListener("submit", () => {
+searchBtn.addEventListener("click", () => {
+  cityInfoContainer.innerHTML = "";
   resolve(searchBar.value);
+  searchBar.value = "";
+});
+
+searchBar.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    cityInfoContainer.innerHTML = "";
+    resolve(searchBar.value);
+    searchBar.value = "";
+  }
 });
